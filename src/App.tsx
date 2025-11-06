8721c5183bc2675690f1260979c2db07a4b33c56
Mehnaaz Ansari <naazzanhem@gmail.com>
2025-11-06 15:04:46 +0530

    chore(ci):add github deploye action

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+
 import './App.css'
 import "tailwindcss"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+   return (
     <>
       <h1 className = "mb-4">Hello Mehnaaz</h1>
       <p>Mehnaaz Zuabir Ahmed</p>
