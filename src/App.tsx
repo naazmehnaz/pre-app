946a3730176076a4353798abbf64e2044550536a
Mehnaaz Ansari <naazzanhem@gmail.com>
2025-11-06 14:22:06 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import "tailwindcss"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className = "mb-4">Hello Mehnaaz</h1>
+      <p>Mehnaaz Zuabir Ahmed</p>
+    </>
+  )
+}
+
+export default App
